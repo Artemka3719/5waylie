02bd4defbce0d8aa29cb9b358d8d13cb756ecce6
Artemka3719 <109282937+Artemka3719@users.noreply.github.com>
2022-08-11 06:27:32 +0700

    Create 5waylie.lua

---
+++ 5waylie.lua
@@ -0,0 +1,44 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("5waylie [BETA]", "RJTheme7")
+
+local Tab = Window:NewTab("Scripts")
+
+local Section = Tab:NewSection("Mode")
+
+Section:NewButton("MM2", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/DihfjHj2'))()
+end)
+
+Section:NewButton("Blox Fruits", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/GooD1020/GooD1020/main/README.md'))()
+end)
+
+Section:NewButton("Mining Sim2", "ButtonInfo", function()
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/RunDTM/miningsim2/main/rewrite.lua')()
+end)
+
+Section:NewButton("CHAOS", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1111-ssss/Chaos_Script/main/Chaos_script.txt'))()
+end)
+
+Section:NewButton("Epic Minigames", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/YePwz5u5", true))()
+end)
+
+Section:NewButton("Rogue Demon", "ButtonInfo", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/itsyouranya/free/main/roguedemon.lua'),true))()
+end)
+
+Section:NewButton("PSX", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/BdvUGb2q"))()
+end)
+
+Section:NewButton("Slayers Unleashed", "ButtonInfo", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/SlayersChanger'),true))()
+end)
+
+Section:NewButton("King Legasy", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hajibeza/RIPPER-HUB/main/King%20Leagacy"))()
+end)
+
