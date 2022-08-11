8d0903fc5f3e2830faac2f07a394e89d05f882b7
Artemka3719 <109282937+Artemka3719@users.noreply.github.com>
2022-08-11 08:37:14 +0700

    Update 5waylie.lua

--- 5waylie.lua
+++ 5waylie.lua
@@ -303,7 +303,6 @@ Section:NewButton("FPS+", "ButtonInfo", function()
     for I,V in pairs(workspace:GetDescendants()) do
     if (V.ClassName == "Part" or V.ClassName == "WedgePart") then
         V.Material = "SmoothPlastic"
-    end
 end)
 
 local Section = Tab:NewSection("UPDATE")
