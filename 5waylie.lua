e1b1924691db7e08955b13334ac31e06b9922f83
Artemka3719 <109282937+Artemka3719@users.noreply.github.com>
2022-08-11 08:31:11 +0700

    Update 5waylie.lua

--- 5waylie.lua
+++ 5waylie.lua
@@ -300,32 +300,14 @@ Section:NewButton("Rejoin Game", "ButtonInfo", function()
 end)
 
 Section:NewButton("FPS+", "ButtonInfo", function()
-    -- Made by RIP#6666
-_G.Settings = {
-    Players = {
-        ["Ignore Me"] = true, -- Ignore your Character
-        ["Ignore Others"] = true -- Ignore other Characters
-    },
-    Meshes = {
-        Destroy = false, -- Destroy Meshes
-        LowDetail = true -- Low detail meshes (NOT SURE IT DOES ANYTHING)
-    },
-    Images = {
-        Invisible = true, -- Invisible Images
-        LowDetail = false, -- Low detail images (NOT SURE IT DOES ANYTHING)
-        Destroy = false, -- Destroy Images
-    },
-    Other = {
-        ["No Particles"] = true, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparkles
-        ["No Camera Effects"] = true, -- Disables all PostEffect's (Camera/Lighting Effects)
-        ["No Explosions"] = true, -- Makes Explosion's invisible
-        ["No Clothes"] = true, -- Removes Clothing from the game
-        ["Low Water Graphics"] = true, -- Removes Water Quality
-        ["No Shadows"] = true, -- Remove Shadows
-        ["Low Rendering"] = true, -- Lower Rendering
-        ["Low Quality Parts"] = true -- Lower quality parts
-    }
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/FPSBooster.lua"))()
+    for I,V in pairs(workspace:GetDescendants()) do
+    if (V.ClassName == "Part" or V.ClassName == "WedgePart") then
+        V.Material = "SmoothPlastic"
+    end
 end)
 
+local Section = Tab:NewSection("UPDATE")
+
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Release 08/11/22,Latest updates 08/11/22"}, function(currentOption)
+    print(currentOption)
+end)
