2040e7f280e6993fd48d51ae3cffba7853dc20a3
Artemka3719 <109282937+Artemka3719@users.noreply.github.com>
2022-08-11 08:43:06 +0700

    Update 5waylie.lua

--- 5waylie.lua
+++ 5waylie.lua
@@ -303,5 +303,8 @@ Section:NewButton("FPS+", "ButtonInfo", function()
     for I,V in pairs(workspace:GetDescendants()) do
     if (V.ClassName == "Part" or V.ClassName == "WedgePart") then
         V.Material = "SmoothPlastic"
+    end
+end
+
 end)
 
